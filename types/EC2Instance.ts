3a680b55d6c73564403201b31dd19639e71841d7
sonjungchan <rnrmfjc@gmail.com>
2025-08-19 22:06:04 +0900

    feat(ec2): add EC2 price fetching for agents

---
+++ types/EC2Instance.ts
@@ -0,0 +1,11 @@
+export interface EC2Instance {
+    instanceType: string;
+    vcpu: string;
+    memory: string;
+    storage: string;
+    networkPerformance: string;
+    pricePerHour: string;
+    description: string;
+    location: string;
+    operatingSystem: string;
+}
